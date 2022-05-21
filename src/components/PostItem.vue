d937a955b7525532ec7d56ac3afcbe0785d4b094
Jul Bar <barankovauv@gmail.com>
2022-05-21 08:51:00 +0300

    commit

---
+++ src/components/PostItem.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="post">
+        <div>Название: {{ post.title }}</div>
+        <div>Название: {{ post.body }}</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            post: {
+                type: Object,
+                required: true
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .post {
+        padding: 15px;
+        border: 2px solid darkolivegreen;
+        margin-top: 15px;
+    }
+</style> 
